b2bce389f9d16c2128a077b04b48bf2c989dfa56
Xoan <Xoan@Xoan-PC>
2017-09-11 19:12:48 +0200

    Module 1 - assignment 1

--- UCSDUnfoldingMaps/src/module1/HelloWorld.java
+++ UCSDUnfoldingMaps/src/module1/HelloWorld.java
@@ -65,7 +65,7 @@ public class HelloWorld extends PApplet
 		// The 6th argument specifies the map provider.  
 		// There are several providers built-in.
 		// Note if you are working offline you must use the MBTilesMapProvider
-		map1 = new UnfoldingMap(this, 50, 50, 350, 500, provider);
+		map1 = new UnfoldingMap(this, 25, 25, 350, 550, provider);
 
 		// The next line zooms in and centers the map at 
 	    // 32.9 (latitude) and -117.2 (longitude)
@@ -74,16 +74,24 @@ public class HelloWorld extends PApplet
 		// This line makes the map interactive
 		MapUtils.createDefaultEventDispatcher(this, map1);
 		
-		// TODO: Add code here that creates map2 
-		// Then you'll modify draw() below
+		map2 = new UnfoldingMap(this, 425, 25, 350, 550, provider);
+		
+		// The next line zooms in and centers the map at 
+	    // 42.32 (latitude) and -8.79 (longitude) BUEU
+	    map2.zoomAndPanTo(zoomLevel, new Location(42.32f, -8.79f));
+	    
+		// This line makes the map interactive
+		MapUtils.createDefaultEventDispatcher(this, map2);
 
 	}
 
 	/** Draw the Applet window.  */
 	public void draw() {
 		// So far we only draw map1...
-		// TODO: Add code so that both maps are displayed
 		map1.draw();
+		
+		//draw map2
+		map2.draw();
 	}
 
 	
